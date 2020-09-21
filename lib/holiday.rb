2ced1cf142c2e37f76457a2abd3b5c1c1474556e
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-21 00:30:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,18 +54,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |season, data|
-    puts "#{season.to_s.capitalize!}:"
-    data.each do |holiday, supply|
-      array = holiday.to_s.split("_")
-      final_holiday = []
-        array.each do |x|
-          final_holiday << x.capitalize!
-        end
-      holiday = final_holiday.join(" ")
-
-      supply = supply.join(", ")
-      puts "  #{holiday}: #{supply}"
+  holiday_hash.each do |season, holidays|
+    
 end
 
 def all_holidays_with_bbq(holiday_hash)
