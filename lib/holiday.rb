7585dea4bbf473a8430abf07a19fcc836df0cfb0
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-21 00:33:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,5 +67,6 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
+      holiday if supplies.include?("BBQ")
       
 end
