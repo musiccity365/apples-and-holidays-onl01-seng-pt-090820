8c0ab9312c4f7d34b948ba4d56293a3a23de9658
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-21 00:31:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
-    
+    holidays.each do |holiday, supplies|
+      
 end
 
 def all_holidays_with_bbq(holiday_hash)
